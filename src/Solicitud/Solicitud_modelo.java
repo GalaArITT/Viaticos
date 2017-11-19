2cbbeed94486555c31bcf1c0e97c808abfe1f4a5
GalaArITT <oljagalavizar@ittepic.edu.mx>
2017-11-18 20:47:20 -0700

    Agregar nuevos archivos

--- src/Solicitud/Solicitud_modelo.java
+++ src/Solicitud/Solicitud_modelo.java
@@ -5,10 +5,37 @@
  */
 package Solicitud;
 
+import java.util.Date;
+
 /**
  *
  * @author oliver
  */
 public class Solicitud_modelo {
+    private String tipoSolicitud;
+    private String lugar;
+    private String actividad;
+    private int idUsuario;
+    private int idProducto;
+    private String pernoctado;
+    private Date fechaSalida;
+    
+    public Solicitud_modelo() {
+        
+    }
+    public Solicitud_modelo(String tipoSolicitud, String lugar, String actividad, 
+            int idUsuario,int idProducto, String pernoctado, Date fechaSalida) {   
+        this.tipoSolicitud=tipoSolicitud;
+        this.lugar=lugar;
+        this.actividad=actividad;
+        this.idUsuario=idUsuario;
+        this.idProducto=idProducto;
+        this.pernoctado=pernoctado;
+        this.fechaSalida=fechaSalida;
+    }
+    
+    public void insertaSolicitud(){
+        
+    }
     
 }
